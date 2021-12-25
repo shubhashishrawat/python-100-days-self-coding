c278d0fcaa9e9053436f27b9dff6388043bcf9a4
shubhashish rawat <87768240+shubhashishrawat@users.noreply.github.com>
2021-12-25 14:55:54 +0530

    commit

--- day 2/start/exercise2.2.py
+++ day 2/start/exercise2.2.py
@@ -1,2 +1,5 @@
 input_two_number = input("enter the two digit nummber:")
-split(input_two_number)
+first_digit_number = input_two_number[0]
+second_digit_number = input_two_number[1]
+sum = (int(first_digit_number) + int(second_digit_number))
+print(sum)
