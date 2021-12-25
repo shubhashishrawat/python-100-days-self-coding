7f5a98e1a6d9d104b45a27bf13a192882c576ef3
shubhashish rawat <87768240+shubhashishrawat@users.noreply.github.com>
2021-12-25 17:18:49 +0530

    commit

---
+++ yearold.py
@@ -0,0 +1,8 @@
+age = input("enter your age")
+age_as_int = int(age)
+years_remaining = 90 - age_as_int
+months_remaining = years_remaining*12
+days_remaining = years_remaining*365
+weeks_remaining = years_remaining*52
+print(
+    "you have months {months_remaining}, days {days_remaining} ,weeks{weeks_remaining}")
