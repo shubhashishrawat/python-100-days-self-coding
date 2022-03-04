639ec4844318e30b3a64cafdf90fa1419550cca4
shubhashish rawat <87768240+shubhashishrawat@users.noreply.github.com>
2022-03-04 21:13:27 +0530

    commit

--- day 5/5.1.py
+++ day 5/5.1.py
@@ -1,9 +1,14 @@
-student_height = int(input("enter the height of the students : "))
+student_height = input("enter the height of the students : ").split()
+for n in range(0, len(student_height)):
+    student_height[n] = int(student_height[n])
+print(student_height)
 total_height = 0
 for height in student_height:
     total_height += height
-    print(total_height)
+print(total_height)
 number_of_student = 0
 for student in student_height:
     number_of_student += 1
-    print(number_of_student)
+print(number_of_student)
+avg = round(total_height / number_of_student)
+print(avg)
